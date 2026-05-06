632d43c69e795674f2c2ded46000e898033d71c7
Emaa <133162428+Emaachaimae@users.noreply.github.com>
2026-05-06 15:50:56 +0100

    implment the home page

---
+++ app/(tabs)/settings2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const settings2 = () => {
+  return (
+    <View>
+      <Text>settings2</Text>
+    </View>
+  );
+};
+
+export default settings2;
